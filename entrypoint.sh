004c0331d5db8bea87f9d5ce7f91501873b2f167
WillBruce <william.bruce@voicescape.com>
2026-03-30 15:29:46 +0100

    IN-203-Update `entrypoint.sh`

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 set -e
 
-python3 /upload_to_confluence.py \
-  --root-page-id "${INPUT_ROOT_PAGE_ID}" \
-  --root-dir "${INPUT_ROOT_DIR}" \
-  --space "${INPUT_SPACE}"
+python3 /upload_to_confluence.py "$@"
